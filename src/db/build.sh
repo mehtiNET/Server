a37820fb0e0621ad0bc26e1ff0f6093a83329cef
Kevin Ryser <kevin.ryser@koweb.ch>
2014-11-24 10:40:11 +0100

    - Clean unused files

--- src/db/build.sh
+++
@@ -1,5 +0,0 @@
-echo "### Nettoyage avant compilation ###"
-gmake clean
-echo "### Compilation - DB ###"
-gmake default > ../"Rapport de compilation DB.txt" 2> ../"Rapport d'erreurs DB.txt"
-echo "### Fin de la compilation - DB ###"
